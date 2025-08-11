234559436b8cdf9ea45c9f5f8b063f84d5364737
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-08-11 19:33:01 -0400

    Commit Inicial

---
+++ src/main/java/com/alura/desafio_forohub/DesafioForohubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.desafio_forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioForohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioForohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/desafio_forohub/DesafioForohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.desafio_forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioForohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
